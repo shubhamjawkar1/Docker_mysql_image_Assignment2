2d51be77f8ed9ef0a0393ce030b97dcd4fddfb47
shubham jawkar <84671309+shubhamjawkar1@users.noreply.github.com>
2022-01-06 21:54:50 +0530

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,5 @@
+CREATE TABLE Student(Name varchar(20), Roll_No integer, Address varchar(40), Mobile_Number bigint primary key NOT NULL, PAN_Number varchar(10));
+
+INSERT INTO studentData values('Shubham', 01, 'Khamgaon', 9999999999, 'ABC101010');
+INSERT INTO studentData values('Ram', 02, 'Pune', 8888888888, 'XYZ020202');
+INSERT INTO studentData VALUES('Sham', 03, 'Mumbai', 777777777, 'PQR030303');
